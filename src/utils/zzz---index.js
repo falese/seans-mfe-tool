0eb3e57a40724b4386e354c304047d2025dea30f
Sean Falese <falese@gmail.com>
2024-11-23 00:22:18 -0500

    cleanup

--- src/utils/zzz---index.js
+++
@@ -1,9 +0,0 @@
-const { DatabaseGenerator } = require('./databaseGenerator');
-const { ControllerGenerator } = require('./controllerGenerator');
-const { generateRoutes } = require('./routeGenerator');
-
-module.exports = {
-  DatabaseGenerator,
-  ControllerGenerator,
-  generateRoutes
-};
